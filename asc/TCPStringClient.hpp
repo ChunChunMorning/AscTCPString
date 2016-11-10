d120bd10f3fdc274e551dab1068138b0ffacbed9
Asachun <chuchuchu.chunchun@gmail.com>
2016-11-10 17:39:02 +0900

    Implement readAll.

--- asc/TCPStringClient.hpp
+++ asc/TCPStringClient.hpp
@@ -5,6 +5,12 @@ namespace asc
 {
 	using namespace s3d;
 
+	/// <summary>
+	/// 文字列を送受信可能な TCPClient
+	/// </summary>
+	/// <remarks>
+	/// 送受信は UTF-8 で行います。
+	/// </remarks>
 	class TCPStringClient : public TCPClient
 	{
 	private:
@@ -13,6 +19,18 @@ namespace asc
 
 	public:
 
+		/// <summary>
+		/// 1 文字読み込みます。
+		/// </summary>
+		/// <param name="to">
+		/// 読み込み先
+		/// </param>
+		/// <returns>
+		/// 読み込みに成功すれば true
+		/// </returns>
+		/// <remarks>
+		/// 日本語などの 1 バイトではない文字は正しく扱えません。
+		/// </remarks>
 		bool readChar(wchar& to)
 		{
 			if (m_buffer.length() > 0)
@@ -30,6 +48,21 @@ namespace asc
 			return true;
 		}
 
+		/// <summary>
+		/// 文字数を指定して文字列を読み込みます。
+		/// </summary>
+		/// <param name="length">
+		/// 読み込む文字数
+		/// </param>
+		/// <param name="to">
+		/// 読み込み先
+		/// </param>
+		/// <returns>
+		/// 読み込みに成功すれば true
+		/// </returns>
+		/// <remarks>
+		/// 日本語などの 1 バイトではない文字は正しく扱えません。
+		/// </remarks>
 		bool readString(size_t length, String& to)
 		{
 			if (m_buffer.length() >= length)
@@ -59,12 +92,39 @@ namespace asc
 			return false;
 		}
 
+		/// <summary>
+		/// 1 行読み込みます。
+		/// </summary>
+		/// <param name="to">
+		/// 読み込み先
+		/// </param>
+		/// <returns>
+		/// 読み込みに成功すれば true
+		/// </returns>
+		/// <remarks>
+		/// 日本語などの 1 バイトではない文字も扱えます。
+		/// </remarks>
 		bool readLine(String& to)
 		{
-			return readUntil(to, '\n');
+			return readUntil('\n', to);
 		}
 
-		bool readUntil(String& to, char end)
+		/// <summary>
+		/// 指定した文字が来るまで読み込みます。
+		/// </summary>
+		/// <param name="end">
+		/// 指定する文字
+		/// </param>
+		/// <param name="to">
+		/// 読み込み先
+		/// </param>
+		/// <returns>
+		/// 読み込みに成功すれば true
+		/// </returns>
+		/// <remarks>
+		/// 日本語などの 1 バイトではない文字も扱えます。
+		/// </remarks>
+		bool readUntil(char end, String& to)
 		{
 			for (;;)
 			{
@@ -84,6 +144,48 @@ namespace asc
 			return true;
 		}
 
+		/// <summary>
+		/// 文字を可能な限り読み込みます。
+		/// </summary>
+		/// <param name="to">
+		/// 読み込み先
+		/// </param>
+		/// <returns>
+		/// 1 文字でも読み込みに成功すれば true
+		/// </returns>
+		/// <remarks>
+		/// 日本語などの 1 バイトではない文字も扱えます。
+		/// </remarks>
+		bool readAll(String& to)
+		{
+			for (;;)
+			{
+				char character;
+
+				if (!read(character))
+					break;
+
+				m_buffer.push_back(character);
+			}
+
+			to = FromUTF8(m_buffer);
+			auto success = m_buffer.empty();
+			m_buffer.clear();
+			return success;
+		}
+
+		/// <summary>
+		/// 文字列を送信します。
+		/// </summary>
+		/// <param name="data">
+		/// 送信する文字列
+		/// </param>
+		/// <returns>
+		/// 送信に成功すれば true
+		/// </returns>
+		/// <remarks>
+		/// 日本語などの 1 バイトではない文字も扱えます。
+		/// </remarks>
 		bool sendString(const String& data)
 		{
 			const auto str = ToUTF8(data);
